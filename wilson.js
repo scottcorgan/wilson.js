3b19ba224f91aac187b011f2f1985912eb01230a
Scott Corgan <scott@sevenly.org>
2012-04-06 09:12:33 -0700

    refactor

--- wilson.js
+++ wilson.js
@@ -5,17 +5,21 @@ var wilson = (function(contenxt){
     return ((p + 1.9208) / (p + n) - 1.96 * Math.sqrt((p * n) / (p + n) + 0.9604) / (p + n)) / (1 + 3.8416 / (p + n));
   }
   
-  //
+  // calculate lower bound based on passing an array of objects
   var calcArr = function(arr){
     
   }
   
-  //
+  // calculate lower bound based on passing an object
   var calcObj = function(obj){
-    
+    var pos = obj.positive || 0,
+        neg = obj.negative || 0;
+        
+    //
+    return calculate(pos, neg);
   }
   
-  //
+  // sort the array (helper function
   var sort = function(arr){
     
   }
@@ -23,11 +27,8 @@ var wilson = (function(contenxt){
   // return 
   return {
     
-    getLowerBound : function(){
-      var arg = arguments[0];
-      
-      //
-      return calcObj(arg);
+    getLowerBound : function(obj){
+      return calcObj(obj);
     },
     
     score : function(){
