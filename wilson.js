c0acfcd576a33e42adc96a5e7618fb963663ee13
Scott Corgan <scott@sevenly.org>
2012-04-06 09:03:43 -0700

    skeleton of wilson.js

---
+++ wilson.js
@@ -0,0 +1,39 @@
+var wilson = (function(contenxt){
+  
+  // wilson equation
+  var calculate = function(p, n){
+    return ((p + 1.9208) / (p + n) - 1.96 * Math.sqrt((p * n) / (p + n) + 0.9604) / (p + n)) / (1 + 3.8416 / (p + n));
+  }
+  
+  //
+  var calcArr = function(arr){
+    
+  }
+  
+  //
+  var calcObj = function(obj){
+    
+  }
+  
+  //
+  var sort = function(arr){
+    
+  }
+  
+  // return 
+  return {
+    
+    getLowerBound : function(){
+      var arg = arguments[0],
+          func = function(){};
+      
+      //
+      arg.length && (func = calcArr); // array of objects
+      !arg.length && (func = calcObj); // object
+      
+      return func(arg);
+    }
+    
+  }
+
+})(this);
